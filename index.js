b2bb22104aca9f4465781a5510fc3c9a7d7db64d
Erika Curry-Elrod <ecurryelrod@gmail.com>
2022-01-16 16:23:02 -0700

    commit tree fb21b40f69582bd5ace8859c756074e792cd27bd

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your functions here
+
+function map(sourceArray, callback) {
+    let newArray = [];
+    for (const elem of sourceArray) {
+        newArray.push(callback(elem));
+    };
+    return newArray;
+};
+
+function reduce(sourceArray, callback, startValue) {
+    let total;
+    if (startValue) {
+        total = startValue;
+        for (const elem of sourceArray) {
+            total = callback(elem, total);
+        };
+        return total;
+    } else {
+        total = sourceArray[0];
+        for (let i = 1; i < sourceArray.length; i++) {
+            total = callback(sourceArray[i], total);
+        };
+        return total;
+    };
+};
